dd501894604bd4a657d3bb56da64945e9d9a6173
dev4589 <86591336+dev4589@users.noreply.github.com>
2021-07-20 12:45:48 +0530

    0

--- resources/views/admin/admincategory.blade.php
+++ resources/views/admin/admincategory.blade.php
@@ -101,6 +101,61 @@ if(!Session::has('adminemail'))
                     <input type="submit" name="submit" id="submit_button" class="btn btn-success" value="Add" />
                     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
                 </div>
+<div class="container-fluid">
+    <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary">Manage
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addcategory">
+                Add Category
+            </button>
+
+            <!-- <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#z">
+            Add Menu 
+        </button> -->
+        </h6>
+    </div>
+
+    <div class="card shadow mb-4">
+        <div class="card-body">
+
+
+            <span>
+                {{$results->links()}}
+            </span>
+            <br><br>
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0" style="padding:50px;">
+                    <thead>
+                        <tr>
+                        <tr>
+                            <th>Cat_Name </th>
+                            <th>Cat_Image</th>
+                            <th>Action</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($results as $r)
+
+                        <tr class="odd gradeX">
+
+                            <td style='width:200px;'>{{$r['cat_name']}}</td>
+                            <td><img src="category_images/{{$r['cat_img']}}" style='height:130px;width:180px;'>
+                            <td class="center">
+                                
+                                <a href="{{ url('admineditcategory',$r['cat_id']) }}"><button type="button"
+                                    class="btn btn-success">Edit</button></a>
+                            
+                                <a href="{{url('deletecategory/'.$r['cat_id'])}}"><button type="button"
+                                    class="btn btn-danger">Delete</button></a>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+
+                <style>
+
+                </style>
             </div>
         </form>
     </div>
